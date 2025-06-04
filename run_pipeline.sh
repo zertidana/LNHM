2af185914b07c082cfe0ee5cc9c9dddc0caad0ca
xaviachristi <xaviachristi@gmail.com>
2025-06-04 17:50:45 +0100

    Add bash script for pipeline

---
+++ run_pipeline.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Run full ETL pipeline.
+
+echo "🔧 Activating virtual environment..."
+
+source venv/bin/activate
+
+echo "🚰 Extracting 🚰"
+python extract.py
+
+echo "🧹 Transforming 🧹"
+python transform.py
+
+echo "📥 Loading 📥"
+python load.py
+
+echo "✅ ETL Pipeline complete ✅"

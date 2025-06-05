6325fac22e6004581553a10e6facb4e1ef40b4f5
Xac <parnellxavier@gmail.com>
2025-06-05 13:37:47 +0100

    Refactor: generalise insert_transformed_data.py

--- pipeline/etl_controller.py
+++ pipeline/etl_controller.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import pandas as pd
 from extract import PlantAPIClient
 from transform import clean_dataframe
-from load import insert_transformed_dataframe
+from load import insert_transformed_data
 
 from utilities import set_logger
 
@@ -17,7 +17,7 @@ def run_pipeline() -> None:
     plant_data = client.get_all_plants()
     plant_df = pd.DataFrame.from_dict(plant_data)
     transformed_dataframe = clean_dataframe(plant_df)
-    insert_transformed_dataframe(transformed_dataframe)
+    insert_transformed_data(transformed_dataframe)
 
 
 if __name__ == "__main__":

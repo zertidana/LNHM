897cbce56b3c82b7aad888f929b06595089951dc
Xac <parnellxavier@gmail.com>
2025-06-06 17:11:32 +0100

    Adjust lambder handler test to reflect changes

--- pipeline/test_lambda_handlers.py
+++ pipeline/test_lambda_handlers.py
@@ -1,11 +1,12 @@
 """A script to test the plant monitoring system lambda handlers."""
 from unittest.mock import MagicMock, patch
 from lambda_handlers import etl_lambda_handler
+import pandas as pd
 
 
 @patch("lambda_handlers.run_pipeline")
 def test_lambda_handler_success(mock_run_pipeline):
-    mock_run_pipeline.return_value = None
+    mock_run_pipeline.return_value = pd.DataFrame()
 
     event = {}
     context = MagicMock()
@@ -15,3 +16,4 @@ def test_lambda_handler_success(mock_run_pipeline):
 
     assert response["statusCode"] == 200
     assert response["body"] == "ETL pipeline executed successfully."
+    assert response["error_data"] == []
